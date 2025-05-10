f3b45059e1aba7902f9203ffc789a25dd44322d5
hamelman <hamelman.sean@gmail.com>
2025-05-10 16:32:41 -0400

    reveal

---
+++ reveal.js
@@ -0,0 +1,12 @@
+const fadeElems = document.querySelectorAll('.fade-in-up');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1 });
+
+fadeElems.forEach(el => observer.observe(el));
