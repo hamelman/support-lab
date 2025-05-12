344c088806f088e451566ac157d3542e64837552
hamelman <hamelman.sean@gmail.com>
2025-05-12 18:18:53 -0400

    update to match version of google sheets

--- functions/edison-rag.js
+++ functions/edison-rag.js
@@ -15,11 +15,9 @@ exports.handler = async function (event, context) {
     const { GoogleSpreadsheet } = require("google-spreadsheet");
     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
 
-    await doc.authenticate({
-      credentials: {
-        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      },
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     });
 
     await doc.loadInfo();
